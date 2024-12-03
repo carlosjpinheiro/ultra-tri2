e7cf8e2a9c4fee2c40ffb8438c2d1816f2894bc0
Eric Castoldi <eric.castoldi@gmail.com>
2024-12-03 19:20:31 -0300

    Routes

--- src/router.js
+++ src/router.js
@@ -27,7 +27,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory('~ultrat09'),
+    history: createWebHistory('/~ultrat09/'),
     routes,
     scrollBehavior(to, from, savedPosition) {
         if (to.hash) {
